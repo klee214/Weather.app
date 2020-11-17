68d9d017052a9d813594d723671c278e7a14231d
klee214 <klee214@myseneca.ca>
2020-11-17 17:19:30 -0500

    create: separate client UI and server side, move all UI to client

---
+++ weather-spa-ui/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css'
+
+const Footer = (props) => {
+  return (
+    <div className={styles.footer}>
+        <p>Created by Kimin Lee</p>
+    </div>
+  );
+}
+
+export default Footer;
