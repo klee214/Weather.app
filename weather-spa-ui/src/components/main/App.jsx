fe04a1ac62cb12bd2193394af873aaf5ee4f6eb6
klee214 <klee214@myseneca.ca>
2020-11-17 17:35:54 -0500

    rollback

--- weather-spa-ui/src/components/main/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
-
-const App = () => {
-  return <div className="App"></div>;
-};
-
-export default App;
